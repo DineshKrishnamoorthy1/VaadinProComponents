2006c10913b5575d5fc5b6750db522d3c5333d45
dkrish <dinesh.krishna@zucisystems.com>
2022-03-15 22:16:58 +0530

    15.03.2022-Map-commit-10.00PM

--- src/main/java/com/example/application/views/spreadsheet/Sheet.java
+++ src/main/java/com/example/application/views/spreadsheet/Sheet.java
@@ -1,19 +1,13 @@
 package com.example.application.views.spreadsheet;
 
 import com.vaadin.addon.spreadsheet.Spreadsheet;
-import com.example.application.views.MainLayout;
-import com.vaadin.addon.spreadsheet.Spreadsheet;
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route(value = "sheet")
 public class Sheet extends Spreadsheet {
-    public Sheet() {
+    public Spreadsheet Sheet() {
         Spreadsheet spreadsheet = new Spreadsheet();
-        VerticalLayout layout = new VerticalLayout();
-
-        layout.add(String.valueOf(spreadsheet));
+        return spreadsheet;
     }
 
 }
